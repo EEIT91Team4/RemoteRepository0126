48d0d9ee2f4d39a49c38c313610573a3aeff0025
Vel <Vel@EEIT-PC>
2017-01-26 15:55:35 +0800

    CustomerDAO changed back and forth

--- LabWebServlet/src/main/java/model/CustomerDAO.java
+++ LabWebServlet/src/main/java/model/CustomerDAO.java
@@ -5,6 +5,6 @@ public interface CustomerDAO {
 	public abstract CustomerBean select(String custid);
 	public abstract boolean update(byte[] password, String email,
 			java.util.Date birth, String custid);
-	//WHATEVER WORKS!
-	//THIS IS ALWAYS IN MY FAVOR! 
+
+
 }
