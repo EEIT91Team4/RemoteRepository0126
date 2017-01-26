10f1a03a0c34ea7f2f4f0b344e84834c91273e2f
Vel <Vel@EEIT-PC>
2017-01-26 12:11:02 +0800

    Commit Attempt 2 taking flight~

--- LabWebServlet/src/main/java/model/CustomerDAO.java
+++ LabWebServlet/src/main/java/model/CustomerDAO.java
@@ -5,4 +5,5 @@ public interface CustomerDAO {
 	public abstract CustomerBean select(String custid);
 	public abstract boolean update(byte[] password, String email,
 			java.util.Date birth, String custid);
+	//WHATEVER WORKS!
 }
