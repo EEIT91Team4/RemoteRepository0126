cfbfb41c30f87350c55b90d2b6d86e56f604c814
Student <Student@EEIT-PC>
2017-01-26 12:05:14 +0800

    Commit Attempt 3 focusing batteries. We're clear!

--- LabWebServlet/src/main/java/model/CustomerDAO.java
+++ LabWebServlet/src/main/java/model/CustomerDAO.java
@@ -6,5 +6,6 @@ public interface CustomerDAO {
 	public abstract boolean update(byte[] password, String email,
 			java.util.Date birth, String custid);
 	//System.out.println("Whatever's clever!");
+	//Trying this thing again gets old really really fast~~~
 
 }
